08c1a21e6019918df8c0d606794dc42ed11f389a
rxMATTEO <erik_sundberg@mail.ru>
2024-04-20 17:50:22 +0300

    Edit stuff

--- app.config.ts
+++ app.config.ts
@@ -1,25 +1,25 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'Plex-CRM Документация',
     description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    // image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      // twitter: 'nuxt_js',
+      // github: 'nuxt-themes/docus',
+      // nuxt: {
+      //   label: 'Nuxt',
+      //   icon: 'simple-icons:nuxtdotjs',
+      //   href: 'https://nuxt.com'
+      // }
     },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
-    },
+    // github: {
+    //   dir: '.starters/default/content',
+    //   branch: 'main',
+    //   repo: 'docus',
+    //   owner: 'nuxt-themes',
+    //   edit: true
+    // },
     aside: {
       level: 0,
       collapsed: false,
