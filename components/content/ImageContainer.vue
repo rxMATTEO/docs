2a0b48b9e9d81be1d796b66cf7b0fd49cd7fc439
rxMATTEO <erik_sundberg@mail.ru>
2024-04-21 16:16:19 +0300

    Try component props

--- components/content/ImageContainer.vue
+++ components/content/ImageContainer.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-
+defineProps<{
+  imgUrl: string
+}>();
 </script>
 
 <template>
   <div>
-    Это контейнер для изображения
-    <slot />
+    Это контейнер для изображения {{ imgUrl }}
+    <ContentSlot unwrap="button" :use="$slots.description" />
   </div>
 </template>
