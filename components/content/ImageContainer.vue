0218263e0b52ddd172a5ed29efadb420767e6b86
rxMATTEO <erik_sundberg@mail.ru>
2024-04-22 09:42:15 +0300

    Change components

--- components/content/ImageContainer.vue
+++ components/content/ImageContainer.vue
@@ -7,6 +7,7 @@ defineProps<{
 <template>
   <div>
     Это контейнер для изображения {{ imgUrl }}
+    <img :src="imgUrl" alt="image">
     <ContentSlot unwrap="button" :use="$slots.description" />
   </div>
 </template>
